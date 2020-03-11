315a874cc39215f8d34113cf6da7c35e4179b08c
ripleycmd <natalia.marek@outlook.com>
2020-03-11 23:37:47 +0000

    Added Template class for supporting substitutions

--- email_send.py
+++ email_send.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path         #similar to os.path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from']= 'madeupemail1@outlook.com'
-email['to'] = 'madeupemail2@outlook.com'
+email['from']='madeupemail1@outlook.com'
+email['to'] ='madeupemail2@outlook.com'
 email['subject'] = 'Hey you!'
 
-email.set_content('Have a good day!')
+email.set_content(html.substitute(name= 'Pingu'), 'html')   # email.set_content(html.substitute({'name'= 'Pingu'})) if that html file has mltpl variables
 
 with smtplib.SMTP(host='smtp-mail.outlook.com', port=587) as smtp:
     smtp.ehlo()
